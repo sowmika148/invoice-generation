c5bd520e8c03d176fc26dd0ac0ec5fe973ad0a7f
sowmika <sowmika148@gmail.com>
2020-04-18 13:44:59 +0530

    CSS changes

--- src/EmailPopupContent/EmailPopupContent.js
+++ src/EmailPopupContent/EmailPopupContent.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import './Popup.scss';
 import './EmailPopupContent.scss';
+import TextField from "@material-ui/core/TextField";
 
 const EmailPopupContent = (props) => {
 
@@ -8,8 +9,8 @@ const EmailPopupContent = (props) => {
 
   return (
     <div className="email-container">
-      <span>Enter email address</span>
-      <input type="text" onChange={(e) => setEmailAddress(e.target.value)}/>
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+      {/*<input type="text" onChange={(e) => setEmailAddress(e.target.value)}/>*/}
       <div className="submit">
         <button onClick={() => props.submit(emailAddress)}>Next</button>
       </div>
